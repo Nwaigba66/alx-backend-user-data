8b93361de663eebc5f729aec208ac61a7c31f2a2
Nwaigba66 <glorianwaigba@gmail.com>
2024-01-18 22:28:02 +0000

    Added Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module define the authentication
+"""
+from flask import request
+from typing import TypeVar, List
+import re
+
+
+class Auth:
+    """The Auth class to implement authentications
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required
+        """
+        if path is None or not excluded_paths:
+            return True
+        path = path if path.endswith("/") else path + "/"
+        for ex_path in excluded_paths:
+            if re.match(ex_path, path):
+                return False
+            return True
+        
+        def authorization_header(self, request=None) -> str:
+            """Used for Authorization Header
+            """
+            if request is None:
+                return None
+            authorization = request.headers.get("Authorization")
+            if not authorization:
+                return None
+            return authorization
+            
+        def current_user(self, request=None) -> TypeVar('User'):
+            """Get the current user
+            """            
+            return None
