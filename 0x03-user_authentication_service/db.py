392fa6bb2fe0a763bde6ca10f96e13d8f7120104
Nwaigba66 <glorianwaigba@gmail.com>
2024-01-25 22:14:15 +0000

    Added create user, find user and update user files

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -15,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -43,3 +45,48 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database based on the given criteria
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering
+
+        Returns:
+            User: First user found based on the provided criteria
+
+        Raises:
+            NoResultFound: If no result is found
+            InvalidRequestError: If an invalid query argument is passed
+        """
+        invalid_args = set(kwargs) - set(User.__table__.columns.keys())
+        if invalid_args:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user attributes in the database
+
+        Args:
+            user_id (int): ID of the user to update
+            **kwargs: Arbitrary keyword arguments for updating user attributes
+
+        Raises:
+            ValueError: If an invalid argument is passed
+        """
+        user = self.find_user_by(id=user_id)
+
+        # Check for invalid arguments
+        invalid_args = set(kwargs) - set(User.__table__.columns.keys())
+        if invalid_args:
+            raise ValueError
+
+        # Update user attributes
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        # Commit changes to the database
+        self._session.commit()
